0976ebf1c5c2621f029fb8fb7fdb55f3d5052ea0
Ebru Gulec <gulecebrueg@gmail.com>
2025-07-07 04:04:52 +0200

    new version

--- dist/generated/proto/vehicle_update_pb.d.ts
+++ dist/generated/proto/vehicle_update_pb.d.ts
@@ -1,109 +1,57 @@
-import type { GenEnum, GenFile, GenMessage } from "@bufbuild/protobuf/codegenv2";
-import type { Message } from "@bufbuild/protobuf";
-/**
- * Describes the file proto/vehicle_update.proto.
- */
-export declare const file_proto_vehicle_update: GenFile;
-/**
- * @generated from message schema.VehicleUpdate
- */
-export type VehicleUpdate = Message<"schema.VehicleUpdate"> & {
-    /**
-     * describing the exact timestamp a given event was triggered
-     *
-     * @generated from field: int64 event_timestamp_posix = 1;
-     */
-    eventTimestampPosix: bigint;
-    /**
-     * describing the event name
-     *
-     * @generated from field: string event_name = 2;
-     */
-    eventName: string;
-    /**
-     * unique identifier for vehicle
-     *
-     * @generated from field: string license_plate = 3;
-     */
-    licensePlate: string;
-    /**
-     * describing the supplier name of the vehicle
-     *
-     * @generated from field: string supplier_name = 4;
-     */
-    supplierName: string;
-    /**
-     * describing the hub slug where the vehicle is used
-     *
-     * @generated from field: string hub_slug = 5;
-     */
-    hubSlug: string;
-    /**
-     * describing the unique identifier for vehicle
-     *
-     * @generated from field: string vehicle_id = 6;
-     */
-    vehicleId: string;
-    /**
-     * describing the current status of the vehicle
-     *
-     * @generated from field: string operational_status = 7;
-     */
-    operationalStatus: string;
-};
-/**
- * Describes the message schema.VehicleUpdate.
- * Use `create(VehicleUpdateSchema)` to create a new message.
- */
-export declare const VehicleUpdateSchema: GenMessage<VehicleUpdate>;
-/**
- * @generated from enum schema.OperationalStatus
- */
+import { BinaryReader, BinaryWriter } from "@bufbuild/protobuf/wire";
+export declare const protobufPackage = "schema";
 export declare enum OperationalStatus {
-    /**
-     * Indicates that the vehicle requires maintenance or repairs.
-     *
-     * @generated from enum value: MAINTENANCE_REQUIRED = 0;
-     */
+    /** MAINTENANCE_REQUIRED - Indicates that the vehicle requires maintenance or repairs. */
     MAINTENANCE_REQUIRED = 0,
-    /**
-     * Specifies that the vehicle is partially functional but may have limited capabilities.
-     *
-     * @generated from enum value: PARTLY_OPERATIONAL = 1;
-     */
+    /** PARTLY_OPERATIONAL - Specifies that the vehicle is partially functional but may have limited capabilities. */
     PARTLY_OPERATIONAL = 1,
-    /**
-     * Represents that the vehicle is fully functional and available for use.
-     *
-     * @generated from enum value: OPERATIONAL = 2;
-     */
+    /** OPERATIONAL - Represents that the vehicle is fully functional and available for use. */
     OPERATIONAL = 2,
-    /**
-     * Specifies that the vehicle is being transported between locations.
-     *
-     * @generated from enum value: IN_TRANSFER = 3;
-     */
+    /** IN_TRANSFER - Specifies that the vehicle is being transported between locations. */
     IN_TRANSFER = 3,
-    /**
-     * Indicates that the vehicle is currently located at the supplier's facility.
-     *
-     * @generated from enum value: AT_SUPPLIER = 4;
-     */
+    /** AT_SUPPLIER - Indicates that the vehicle is currently located at the supplier's facility. */
     AT_SUPPLIER = 4,
-    /**
-     * Indicates that the vehicle has been reported as stolen.
-     *
-     * @generated from enum value: STOLEN = 5;
-     */
+    /** STOLEN - Indicates that the vehicle has been reported as stolen. */
     STOLEN = 5,
-    /**
-     * Represents that the vehicle is stored and not actively in use.
-     *
-     * @generated from enum value: IN_STORAGE = 6;
-     */
-    IN_STORAGE = 6
+    /** IN_STORAGE - Represents that the vehicle is stored and not actively in use. */
+    IN_STORAGE = 6,
+    UNRECOGNIZED = -1
 }
-/**
- * Describes the enum schema.OperationalStatus.
- */
-export declare const OperationalStatusSchema: GenEnum<OperationalStatus>;
+export declare function operationalStatusFromJSON(object: any): OperationalStatus;
+export declare function operationalStatusToJSON(object: OperationalStatus): string;
+export interface VehicleUpdate {
+    /** describing the exact timestamp a given event was triggered. */
+    eventTimestampPosix: number;
+    /** describing the event name */
+    eventName: string;
+    /** unique identifier for vehicle */
+    licensePlate: string;
+    /** describing the supplier name of the vehicle */
+    supplierName: string;
+    /** describing the hub slug where the vehicle is used */
+    hubSlug: string;
+    /** describing the unique identifier for vehicle */
+    vehicleId: string;
+    /** describing the current status of the vehicle */
+    operationalStatus: string;
+}
+export declare const VehicleUpdate: MessageFns<VehicleUpdate>;
+type Builtin = Date | Function | Uint8Array | string | number | boolean | undefined;
+export type DeepPartial<T> = T extends Builtin ? T : T extends globalThis.Array<infer U> ? globalThis.Array<DeepPartial<U>> : T extends ReadonlyArray<infer U> ? ReadonlyArray<DeepPartial<U>> : T extends {} ? {
+    [K in keyof T]?: DeepPartial<T[K]>;
+} : Partial<T>;
+type KeysOfUnion<T> = T extends T ? keyof T : never;
+export type Exact<P, I extends P> = P extends Builtin ? P : P & {
+    [K in keyof P]: Exact<P[K], I[K]>;
+} & {
+    [K in Exclude<keyof I, KeysOfUnion<P>>]: never;
+};
+export interface MessageFns<T> {
+    encode(message: T, writer?: BinaryWriter): BinaryWriter;
+    decode(input: BinaryReader | Uint8Array, length?: number): T;
+    fromJSON(object: any): T;
+    toJSON(message: T): unknown;
+    create<I extends Exact<DeepPartial<T>, I>>(base?: I): T;
+    fromPartial<I extends Exact<DeepPartial<T>, I>>(object: I): T;
+}
+export {};
